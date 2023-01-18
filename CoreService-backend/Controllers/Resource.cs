7540f8fc0a5160354ca4e3fa5d1e4e9d058b223c
Mateusz <mateusz.moqbil@gmail.com>
2023-01-18 11:46:20 +0100

    Added get user resources route

--- CoreService-backend/Controllers/Resource.cs
+++ CoreService-backend/Controllers/Resource.cs
@@ -12,9 +12,17 @@ namespace CoreService_backend.Controllers
 
         [HttpPost]
         [Route("resources")]
-        public string GetData()
+        public string GetResources()
         {
             _resource.Add(new Models.Resource("FirstResource","127.0.0",1));
+            return JsonConvert.SerializeObject(_resource);
+        }
+
+        [HttpPost]
+        [Route("userresources")]
+        public string GetUserResources([FromBody] Models.User user)
+        {
+            _resource.GetResourcesForUserId(user.Id);
             return JsonConvert.SerializeObject(_resource);
         }
     }

58fd3ec21474ba14c75c5b766f3753163255e17e
Amidass <aa.wrozyna@gmail.com>
2023-02-21 13:42:14 +0100

    Add new part of application - qeueu

---
+++ CoreService-Core/Program.cs
@@ -0,0 +1,10 @@
+using CoreService_Core;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();

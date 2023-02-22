232d0399de8e66b7a47e3bc5a453ac10f6af6fa4
Amidass <aa.wrozyna@gmail.com>
2023-02-22 08:39:50 +0100

    Some changes

---
+++ CoreService-backend/Controllers/ResourceController.cs
@@ -0,0 +1,48 @@
+﻿using CoreService_backend.Dtos;
+using CoreService_backend.Enitities;
+using CoreService_backend.Models.Dtos;
+using CoreService_backend.Services.Api;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+
+namespace CoreService_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResourceController : ControllerBase
+    {
+        private IResourceService _resource;
+
+        [HttpGet]
+        public async Task<IEnumerable<Resource>?> GetResources()
+        {
+            return await _resource.GetResources();
+        }
+
+        [HttpGet]
+        [Route("/{userID:int}")]
+        public async Task<IEnumerable<Resource>?> GetUserResources(int userID)
+        {
+            // add validation user Authentication
+            return await _resource.GetResourcesByUserID(userID);
+        }
+
+        [HttpGet]
+        [Route("/{userID:int}/{resourceID:int}")]
+        public async Task<Resource?> GetResource(int userID, int resourceID)
+        {
+            // add validation user Authentication
+
+            return await _resource.GetResourceById(resourceID);
+        }
+
+        [HttpPost]
+        [Route("/{userID:int}/{resourceID:int}")]
+        public async Task UpdateResource([FromBody] ResourceUpdateDto resource)
+        {
+            // add validation user Authentication
+            await _resource.UpdateResource(resource);
+        }
+    }
+}

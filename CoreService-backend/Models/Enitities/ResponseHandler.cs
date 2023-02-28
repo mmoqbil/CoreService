82d44e5eb130c90b68af0979f7f20bd6d5271771
Amidass <aa.wrozyna@gmail.com>
2023-02-28 11:29:29 +0100

    Added new table to database - Response

---
+++ CoreService-backend/Models/Enitities/ResponseHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CoreService_backend.Enitities;
+
+namespace CoreService_backend.Models.Enitities
+{
+    [Table("Response")]
+    public class ResponseHandler
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Resource))]
+        public string ResourceId { get; set; }
+
+        [Required]
+        public string ResponseStatus { get; set; }
+
+        [MaxLength(100)]
+        public string ErrorMessage { get; set; }
+
+        public int Ping { get; set; }
+
+        [Required]
+        public TimeOnly DateTime { get; set; }
+    }
+}

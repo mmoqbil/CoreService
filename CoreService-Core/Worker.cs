e39c142027aa2337c841559188b466253600e121
Mateusz <mateusz.moqbil@gmail.com>
2023-02-28 09:25:01 +0100

    Queue done

--- CoreService-Core/Worker.cs
+++ CoreService-Core/Worker.cs
@@ -1,13 +1,25 @@
+using CoreService_Core.Data.Model;
+using CoreService_Core.Data.Service;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace CoreService_Core
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly ResourceService _resourceService;
         private HttpClient _client;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, ResourceService resourceService)
         {
             _logger = logger;
+            _resourceService = resourceService;
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
@@ -26,18 +38,29 @@ namespace CoreService_Core
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                var result = await _client.GetAsync("http://www.iamtimcorey.com");
+                foreach (var resource in _resourceService.resources)
+                {
+                    if (resource.TimeLeftSeconds <= 0)
+                    {
+                        resource.TimeLeftSeconds = resource.RepeatSeconds;
+                        var result = await _client.GetAsync(resource.IpAdress);
 
-                if (result.IsSuccessStatusCode)
-                {
-                    _logger.LogInformation("The status code was: {statusCode}, time: {time}", result.StatusCode, DateTime.Now);
+                        if (result.IsSuccessStatusCode)
+                        {
+                            _logger.LogInformation("The status code was: {statusCode}, time: {time}, name: {name}", result.StatusCode, DateTime.Now, resource.Name);
+                        }
+                        else
+                        {
+                            _logger.LogError("The website is down. Status code {StatusCode}", result.StatusCode);
+                        }
+                    }
+                    else
+                    {
+                        _logger.LogInformation("This resource is not ready! timeLeft: {time}",resource.TimeLeftSeconds);
+                        resource.TimeLeftSeconds -= 60;
+                    }
                 }
-                else
-                {
-                    _logger.LogError("The website is down. Status code {StatusCode}", result.StatusCode);
-                }
-                    
-                await Task.Delay(5000, stoppingToken);
+                await Task.Delay(3000, stoppingToken);
             }
         }
     }

17d2666780b340f1364fc3dcfd2dfc4f75790d8b
Mateusz <mateusz.moqbil@gmail.com>
2023-03-01 14:02:17 +0100

    Added SendMailWithError method

--- CoreService-Core/Infrastructure/QueueManager.cs
+++ CoreService-Core/Infrastructure/QueueManager.cs
@@ -3,8 +3,10 @@ using CoreService_Core.Model.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Mail;
 
 namespace CoreService_Core.Infrastructure
 {
@@ -34,6 +36,7 @@ namespace CoreService_Core.Infrastructure
                     else
                     {
                         logger.LogError("the website is down. status code {statuscode}", result.StatusCode);
+                        SendEmailWithError(result.StatusCode,resource.Name);
                     }
                 }
                 else
@@ -42,6 +45,19 @@ namespace CoreService_Core.Infrastructure
                 }
             }
             return availableresources;
+        }
+        public void SendEmailWithError(HttpStatusCode statusCode, string serviceName)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("coreserviceinfo@gmail.com");
+            message.To.Add(new MailAddress("adres@uzytkownika.com"));
+            message.Subject = "Error with service" + serviceName;
+            message.Body = "There is in service: " + serviceName + " With this error: " + statusCode;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("coreserviceinfo@gmail.com", "Coreservice123");
+            client.Send(message);
         }
     }
 }

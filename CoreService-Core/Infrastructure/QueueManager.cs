d3dc460f8236239fbd8d8fe019afc1ce9adfa3b9
Mateusz <mateusz.moqbil@gmail.com>
2023-03-01 13:04:28 +0100

    Added ResourceDto

--- CoreService-Core/Infrastructure/QueueManager.cs
+++ CoreService-Core/Infrastructure/QueueManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,38 @@ namespace CoreService_Core.Infrastructure
 {
     internal class QueueManager
     {
+        private readonly IMapper _mapper;
+        
+        public QueueManager()
+        {
+            
+        }
+
+        public async Task<List<Resource>> checkallavailableresources(resourceservice resourceservice, httpclient client, ilogger<worker> logger)
+        {
+            list<resource> availableresources = new list<resource>();
+            foreach (var resource in resourceservice.resources)
+            {
+                if (resource.timeleftseconds <= 0)
+                {
+                    resource.timeleftseconds = resource.repeatseconds;
+                    availableresources.add(resource);
+                    var result = await client.getasync(resource.ipadress);
+                    if (result.issuccessstatuscode)
+                    {
+                        logger.loginformation("the status code was: {statuscode}, time: {time}, name: {name}", result.statuscode, datetime.now, resource.name);
+                    }
+                    else
+                    {
+                        logger.logerror("the website is down. status code {statuscode}", result.statuscode);
+                    }
+                }
+                else
+                {
+                    resource.timeleftseconds -= 60;
+                }
+            }
+            return availableresources;
+        }
     }
 }
---
+++ CoreService-Core/Model/Dto/ResourceDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using CoreService_Core.Model.Enum;
+
+namespace CoreService_Core.Model.Dto
+{
+    public class ResourceDto
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string UrlAdress { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(IdentityUser))]
+        public string UserId { get; set; }
+
+        [Required]
+        public TimeSpan Refresh { get; set; }
+
+        [Required]
+        public RequestType RequestType { get; set; }
+
+        [Required]
+        public TimeSpan TimeLeft { get; set; }
+
+    }
+}

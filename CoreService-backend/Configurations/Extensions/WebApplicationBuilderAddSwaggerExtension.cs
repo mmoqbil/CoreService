8ff628a779de1c9dd389919cf49933e7e3f8b3ce
Amidass <aa.wrozyna@gmail.com>
2023-03-09 00:20:30 +0100

    Implementation authorization with JWT token in swagger

--- CoreService-backend/Configurations/Extensions/WebApplicationBuilderAddSwaggerExtension.cs
+++ CoreService-backend/Configurations/Extensions/WebApplicationBuilderAddSwaggerExtension.cs
@@ -8,7 +8,37 @@ public static class WebApplicationBuilderAddSwaggerExtension
     {
         builder.Services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoreService", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "My API",
+                Version = "3.0.0"
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Enter your bearer token in the format Bearer <token>",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+
+                    new string[] {}
+                }
+            });
         });
 
         return builder;
